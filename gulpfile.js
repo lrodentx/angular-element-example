a8d468ed5a1c915183968f71e8aae1b4541087fd
Leon Jäkel <info@leon-jaekel.com>
2018-09-04 17:15:20 +0200

    createWidget(baseUrl) method convention added
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   gulpfile.js
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   widget.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 const gulp = require("gulp");
 const concat = require("gulp-concat");
 const gzip = require("gulp-gzip");
+const append = require("gulp-append");
 
 gulp.task("package", () => {
   gulp
-    .src("dist/elements-demo/*.js")
-    .pipe(concat('elements.js'))
+    .src(["dist/elements-demo/*.js", "widget.js"])
+    .pipe(concat("elements.js"))
     .pipe(gzip())
     .pipe(gulp.dest("./"));
-}); 
+});
---
+++ widget.js
@@ -0,0 +1,4 @@
+function createWidget(baseUrl) {
+    window.BASE_URL = baseUrl;
+    return document.createElement('custom-button');
+}
