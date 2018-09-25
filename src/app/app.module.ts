48ea55f5eec75048381a63c446cf5aaa3f2b0dcd
Peter Munsche <xgadpmu@inhouse.ad.in.gad.de>
2018-09-25 18:15:51 +0200

    Hi Leon, viel Spass beim Mergen ;-)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { createCustomElement } from '@angular/elements';
+import { createCustomElement, NgElementConstructor } from '@angular/elements';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
-import { ButtonComponent } from './button/button.component'; 
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [ButtonComponent],
@@ -22,9 +22,12 @@ import { ButtonComponent } from './button/button.component';
 export class AppModule {
   constructor(private injector: Injector) {
     if (customElements.get('custom-button')) { return; }
-    const customButton = createCustomElement(ButtonComponent, {
+    const customButton: NgElementConstructor<HTMLElement> = createCustomElement(ButtonComponent, {
       injector: this.injector
     });
+    /* customButton.prototype.connectedCallback = () => {
+      alert('ICH BIN CONNECTED');
+    }; */
     customElements.define('custom-button', customButton);
   }
 
